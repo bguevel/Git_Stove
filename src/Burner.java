c12b51f5dc0a62d9cad78f8d718e602d275abf81
bguevel <bguevel@mines.edu>
2023-09-09 22:26:00 -0600

    Other files made (Mallorie /m, Ben G).

---
+++ src/Burner.java
@@ -0,0 +1,17 @@
+public class Burner{
+	public enum Temperature{
+		BLAZING, HOT, WARM, COLD;
+	}
+	final int TIME_DURATION=2;
+	Setting mySetting;
+	Temperature myTemperature;
+	public Temperature getMyTemperature() {
+		return myTemperature;
+	}
+	public void plusButton() {
+		
+	}
+	public void minusButton() {
+		
+	}
+}
---
+++ src/Setting.java
@@ -0,0 +1,11 @@
+public enum Setting{
+	OFF("---"), LOW("--+"), MEDIUM("-++"), HIGH("+++");
+	private String value="";
+	Setting(String v){
+		value=v;
+	}
+	public String toString() {
+		return value;
+	}
+	
+}
